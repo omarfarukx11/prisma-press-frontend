2c945a8962d42c3ff24fe79f364b655507256b35
omarfarukx11 <khfarukcht@gmail.com>
2026-08-24 15:54:40 +0600

    env file added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Card } from "@/components/ui/card";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
-    hello world
+    <Card>
+      hello world
+    </Card>
+    <Button size={"lg"} variant={"destructive"} >Click me!</Button>
    </div>
   );
 }
